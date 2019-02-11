e4006050c2b8f4a39e89a001015c52b146d208d1
Brent Schapman <bschapman44@gmail.com>
2019-02-11 14:36:50 -0600

    Added simple counter loop

---
+++ Project2/BeerSong.java
@@ -0,0 +1,18 @@
+
+public class BeerSong {
+
+	public static void main(String[] args) {
+		
+		//public Ninety_Nine_Bottles_of_Beer();
+		
+
+		int Beer = 99;
+		
+		while (Beer >0);
+		
+		System.out.println(Beer + " bottles of beer on the wall, " + Beer + " bottles of beer");
+        Beer = Beer - 1;
+		System.out.println(" Take one down, pass it around, " + Beer + "bottles of beer on the wall");
+	}
+
+}
